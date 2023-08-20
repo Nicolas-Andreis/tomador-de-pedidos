73ceda04aecbcb02b1ee62e94bc46fcd345403fa
Nicolas Andreis <jnandreis@outlook.com>
2023-08-20 17:49:49 -0300

    Primer commit

--- js/main.js
+++ js/main.js
@@ -76,94 +76,124 @@ window.addEventListener('click', (event) => {
 //corazon card
 
 
+//card
+const productosPizzas = [
+    { id: 1, nombre: "muzzarella", precio: 2910, imagen: "./imagenes/pizzas/muzzarella.jpg" },
+    { id: 2, nombre: "fugazza", precio: 3230, imagen: "./imagenes/pizzas/fuggazza.jpg" },
+    { id: 3, nombre: "napolitana", precio: 3300, imagen: "./imagenes/pizzas/napolitana.jpg" },
+    { id: 4, nombre: "provolone", precio: 3490, imagen: "./imagenes/pizzas/provolone.jpg" },
+    { id: 5, nombre: "roquefort", precio: 3490, imagen: "./imagenes/pizzas/roquefort.jpg" },
+    { id: 6, nombre: "aceitunas", precio: 3240, imagen: "./imagenes/pizzas/aceitunas.jpg" },
+    { id: 7, nombre: "muzza y jamon", precio: 3300, imagen: "./imagenes/pizzas/muzzayjamon.jpg" },
+    { id: 8, nombre: "especial", precio: 3620, imagen: "./imagenes/pizzas/especial.jpg" },
+    { id: 9, nombre: "estacione", precio: 3620, imagen: "./imagenes/pizzas/estacione.jpg" },
+    { id: 10, nombre: "j. crudo y rucula", precio: 4080, imagen: "./imagenes/pizzas/jamoncrudoyrucula.jpg" },
+    { id: 11, nombre: "champinones", precio: 3720, imagen: "./imagenes/pizzas/champignones.jpg" },
+    { id: 12, nombre: "alla cardille", precio: 4010, imagen: "./imagenes/pizzas/allacardille.jpg" },
+    { id: 13, nombre: "alla matioli", precio: 3880, imagen: "./imagenes/pizzas/allamattioli.jpg" },
+    { id: 14, nombre: "huevo y cebolla", precio: 3880, imagen: "./imagenes/pizzas/huevoycebolla.jpg" },
+    { id: 15, nombre: "palmitos", precio: 4010, imagen: "./imagenes/pizzas/palmitos.jpg" },
+    { id: 16, nombre: "primavera", precio: 3820, imagen: "./imagenes/pizzas/primavera.jpg" },
+    { id: 17, nombre: "calabresa", precio: 3880, imagen: "./imagenes/pizzas/calabresa.jpg" },
+    { id: 18, nombre: "wok de verduras", precio: 3880, imagen: "./imagenes/pizzas/wokdeverduras.jpg" },
+    { id: 19, nombre: "giuseppe", precio: 3620, imagen: "./imagenes/pizzas/giussepe.jpg" },
+    { id: 20, nombre: "hawaiana", precio: 3880, imagen: "./imagenes/pizzas/hawaiana.jpg" },
+    { id: 21, nombre: "margarita", precio: 3880, imagen: "./imagenes/pizzas/margarita.jpg" },
+    { id: 22, nombre: "alla reina", precio: 4010, imagen: "./imagenes/pizzas/allareina.jpg" },
+    { id: 23, nombre: "cantimpalo", precio: 3880, imagen: "./imagenes/pizzas/cantimpalo.jpg" },
+    { id: 24, nombre: "del mar", precio: 4470, imagen: "./imagenes/pizzas/delmar.jpg" },
+    { id: 25, nombre: "apio y nuez", precio: 3880, imagen: "./imagenes/pizzas/apioynuez.jpg" },
+    { id: 26, nombre: "pollo", precio: 4530, imagen: "./imagenes/pizzas/pollo.jpg" },
+    { id: 27, nombre: "a tu gusto", precio: 4730, imagen: "./imagenes/pizzas/atugusto.jpg" },
+    { id: 28, nombre: "roque y panceta", precio: 4080, imagen: "./imagenes/pizzas/roquefortypanceta.jpg" },
+    { id: 29, nombre: "panceta y huevo", precio: 4470, imagen: "./imagenes/pizzas/pancetayhuevo.jpg" },
+    { id: 30, nombre: "americana", precio: 4730, imagen: "./imagenes/pizzas/americana.jpg" },
+    { id: 31, nombre: "cuatro quesos", precio: 4400, imagen: "./imagenes/pizzas/cuatroquesos.jpg" },
+    { id: 32, nombre: "alla pirata", precio: 4340, imagen: "./imagenes/pizzas/allapirata.jpg" },
+];
 
+const productosMediasPizzas = [
+    { id: 1, nombre: "muzzarella", precio: 1540, imagen: "./imagenes/pizzas/muzzarella.jpg" },
+    { id: 2, nombre: "fugazza", precio: 1700, imagen: "./imagenes/pizzas/fuggazza.jpg" },
+    { id: 3, nombre: "napolitana", precio: 1740, imagen: "./imagenes/pizzas/napolitana.jpg" },
+    { id: 4, nombre: "provolone", precio: 1830, imagen: "./imagenes/pizzas/provolone.jpg" },
+    { id: 5, nombre: "roquefort", precio: 1830, imagen: "./imagenes/pizzas/roquefort.jpg" },
+    { id: 6, nombre: "aceitunas", precio: 1710, imagen: "./imagenes/pizzas/aceitunas.jpg" },
+    { id: 7, nombre: "muzza y jamon", precio: 1740, imagen: "./imagenes/pizzas/muzzayjamon.jpg" },
+    { id: 8, nombre: "especial", precio: 1900, imagen: "./imagenes/pizzas/especial.jpg" },
+    { id: 9, nombre: "estacione", precio: 1900, imagen: "./imagenes/pizzas/estacione.jpg" },
+    { id: 10, nombre: "j. crudo y rucula", precio: 2150, imagen: "./imagenes/pizzas/jamoncrudoyrucula.jpg" },
+    { id: 11, nombre: "champinones", precio: 1950, imagen: "./imagenes/pizzas/champignones.jpg" },
+    { id: 12, nombre: "alla cardille", precio: 2100, imagen: "./imagenes/pizzas/allacardille.jpg" },
+    { id: 13, nombre: "alla matioli", precio: 2040, imagen: "./imagenes/pizzas/allamattioli.jpg" },
+    { id: 14, nombre: "huevo y cebolla", precio: 2040, imagen: "./imagenes/pizzas/huevoycebolla.jpg" },
+    { id: 15, nombre: "palmitos", precio: 2100, imagen: "./imagenes/pizzas/palmitos.jpg" },
+    { id: 16, nombre: "primavera", precio: 2010, imagen: "./imagenes/pizzas/primavera.jpg" },
+    { id: 17, nombre: "calabresa", precio: 2040, imagen: "./imagenes/pizzas/calabresa.jpg" },
+    { id: 18, nombre: "wok de verduras", precio: 2040, imagen: "./imagenes/pizzas/wokdeverduras.jpg" },
+    { id: 19, nombre: "giuseppe", precio: 1910, imagen: "./imagenes/pizzas/giussepe.jpg" },
+    { id: 20, nombre: "hawaiana", precio: 2100, imagen: "./imagenes/pizzas/hawaiana.jpg" },
+    { id: 21, nombre: "margarita", precio: 2040, imagen: "./imagenes/pizzas/margarita.jpg" },
+    { id: 22, nombre: "alla reina", precio: 2100, imagen: "./imagenes/pizzas/allareina.jpg" },
+    { id: 23, nombre: "cantimpalo", precio: 2040, imagen: "./imagenes/pizzas/cantimpalo.jpg" },
+    { id: 24, nombre: "del mar", precio: 2340, imagen: "./imagenes/pizzas/delmar.jpg" },
+    { id: 25, nombre: "apio y nuez", precio: 2040, imagen: "./imagenes/pizzas/apioynuez.jpg" },
+    { id: 26, nombre: "pollo", precio: 2370, imagen: "./imagenes/pizzas/pollo.jpg" },
+    { id: 27, nombre: "a tu gusto", precio: 2470, imagen: "./imagenes/pizzas/atugusto.jpg" },
+    { id: 28, nombre: "roque y panceta", precio: 2150, imagen: "./imagenes/pizzas/roquefortypanceta.jpg" },
+    { id: 29, nombre: "panceta y huevo", precio: 2340, imagen: "./imagenes/pizzas/pancetayhuevo.jpg" },
+    { id: 30, nombre: "americana", precio: 2470, imagen: "./imagenes/pizzas/americana.jpg" },
+    { id: 31, nombre: "cuatro quesos", precio: 2310, imagen: "./imagenes/pizzas/cuatroquesos.jpg" },
+    { id: 32, nombre: "alla pirata", precio: 2280, imagen: "./imagenes/pizzas/allapirata.jpg" },
+];
 
-//prentrega coderhouse  //////////////////////////////////////
-// Función para obtener el nombre del cliente
-function obtenerNombreCliente() {
-    let nombre = prompt(`Ingrese su nombre`);
-    return nombre;
-}
+const productosCombos = [
+    { id: 1, nombre: "combo pirata", precio: 2580, imagen: "./imagenes/burguers/burguerpirata.jpg" },
+    { id: 2, nombre: "combo barbacoa", precio: 2680, imagen: "./imagenes/burguers/burguerbbq.jpg" },
+    { id: 3, nombre: "combo corazon azul", precio: 2680, imagen: "./imagenes/burguers/burgurguercorazonazul.jpg" },
+    { id: 4, nombre: "combo inmortal", precio: 3720, imagen: "./imagenes/burguers/burguerinmortal.jpg" },    
+];
 
-// Función para mostrar el mensaje de despedida
-function mostrarDespedida(nombre) {
-    alert(`Gracias por visitarnos, ${nombre}. ¡Esperamos verte pronto nuevamente!`);
-}
+const productosBurguerSola = [
+    { id: 1, nombre: "combo pirata", precio: 2580, imagen: "./imagenes/burguers/burguerpirata.jpg" },
+    { id: 2, nombre: "combo barbacoa", precio: 2680, imagen: "./imagenes/burguers/burguerbbq.jpg" },
+    { id: 3, nombre: "combo corazon azul", precio: 2680, imagen: "./imagenes/burguers/burgurguercorazonazul.jpg" },
+    { id: 4, nombre: "combo inmortal", precio: 3720, imagen: "./imagenes/burguers/burguerinmortal.jpg" },    
+];
 
-// Función para mostrar el total de la compra
-function mostrarTotalCompra(total) {
-    alert(`El total de su compra es: ${total}`);
-}
+function agregarTarjetas(container, productos) {
+    for (const producto of productos) {
+        let contenedor = document.createElement("div");
+        contenedor.classList.add("card"); // Agregar la clase "card"
 
-// Función para realizar el proceso de compra de pizzas
-function realizarCompra(pizzas) {
-    let totalCompra = 0;
-    let detalleCompra = '';
-    let seguirComprando = true;
+        // Definir el contenido HTML de la tarjeta
+        contenedor.innerHTML = `
+            <img src="${producto.imagen}" alt="">
+            <div class="card-texto">
+                <p>${producto.nombre}</p>
+                <p>$${producto.precio}</p>
+            </div>
+            <div class="card-iconos">
+                <img src="./imagenes/iconos/corazon/heart.png" alt="corazon" class="heart">
+                
+                <div class="contador">
+                    <img src="./imagenes/iconos/basura/trash.png" alt="trash">
+                    <p>0</p>
+                    <img src="./imagenes/iconos/shopping cart/sum.png" alt="+">
+                </div>
+            </div>
+        `;
 
-    do {
-        let compra = parseInt(prompt(`Qué pizza quiere comprar?\nIndique un número 1, 2, 3, 4 (solo números)
-        \n1 Pizza Muzzarella $2530 \n2 Pizza Especial $3150 \n3 Pizza Napolitana $2870 \n4 Pizza Fuggazza $2810`));
-
-        // Recorremos el array de pizzas utilizando un bucle for...of
-        for (let pizza of pizzas) {
-            if (compra === pizza.id) { // Si la compra coincide con el id de la pizza
-                totalCompra += pizza.precio; // Sumamos el precio al total de la compra
-                detalleCompra += `Tu compra de ${pizza.nombre} valor: $${pizza.precio} ha sido exitosa!\n`; // Agregamos el detalle de la compra
-            }
-        }
-
-        // Validamos si el número de compra es inválido
-        if (compra < 1 || compra > pizzas.length) {
-            alert("No disponemos esa variedad o el número ingresado no es válido.");
-        }
-
-        // Preguntamos si el cliente desea seguir comprando
-        seguirComprando = confirm("¿Desea comprar otra pizza?");
-    } while (seguirComprando);
-
-    // Llamamos a la función para mostrar el total de la compra
-    mostrarTotalCompra("$" + totalCompra);
-
-    // Mostramos el detalle de la compra
-    alert(detalleCompra);
-}
-
-// Función para saludar al cliente y comenzar el flujo
-function saludar() {
-    let nombreCliente = obtenerNombreCliente(); // Obtener el nombre del cliente
-    alert(`Hola ${nombreCliente}, Bienvenido a nuestra pizzeria`);
-
-    let decision = prompt(`Indique 1 o 2 (solo números)\n1 Quiero hacer un pedido\n2 No quiero ver opciones`);
-
-    if (decision === "1") { // Si el cliente decide hacer un pedido
-        // Creamos un array de objetos que representan las pizzas disponibles
-        let pizzas = [
-            { id: 1, nombre: 'Pizza Muzzarella', precio: 2530 },
-            { id: 2, nombre: 'Pizza Especial', precio: 3150 },
-            { id: 3, nombre: 'Pizza Napolitana', precio: 2870 },
-            { id: 4, nombre: 'Pizza Fuggazza', precio: 2810 }
-        ];
-
-        alert("Estas son nuestras pizzas:\n");
-
-        // Mostramos las opciones de pizzas disponibles
-        for (let pizza of pizzas) {
-            alert(`${pizza.nombre}           $${pizza.precio}`);
-        }
-
-        // Llamamos a la función para que el cliente realice la compra
-        realizarCompra(pizzas);
-
-    } else if (decision === "2") { // Si el cliente no desea hacer un pedido
-        alert(`Esperamos verte pronto.`);
-    } else {
-        alert("Error, no indicó 1 o 2"); // Si la opción ingresada es inválida
+        container.appendChild(contenedor); // Agregar la tarjeta al contenedor
     }
-
-    // Llamamos a la función para mostrar el mensaje de despedida
-    mostrarDespedida(nombreCliente);
 }
 
-// Llamamos a la función para comenzar el flujo de interacción con el cliente
-saludar();
+const containerPizzas = document.querySelector("#Pizzas");
+const containerMediasPizzas = document.querySelector("#MediasPizzas");
+const containerCombos = document.querySelector("#Combos");
+const containerBurguersSolas = document.querySelector("#BurguersSolas");
+
+agregarTarjetas(containerPizzas, productosPizzas);
+agregarTarjetas(containerMediasPizzas, productosMediasPizzas);
+agregarTarjetas(containerCombos, productosCombos);
+agregarTarjetas(containerBurguersSolas, productosBurguerSola);
